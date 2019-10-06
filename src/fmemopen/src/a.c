727dab0dfba869e49b5d717db9a61af1fa9920d4
skullquake <ockert8080@gmail.com>
2019-10-06 19:37:36 +0200

    .

---
+++ src/fmemopen/src/a.c
@@ -0,0 +1,54 @@
+//$ ./a.out '1 23 43' size=11; ptr=1 529 1849  
+
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define handle_error(msg) \
+    do { perror(msg); exit(EXIT_FAILURE); } while (0)
+
+int main(int argc, char *argv[]) {
+    FILE *out, *in;
+    int v, s;
+    size_t size;
+    char *ptr;
+
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s '<num>...'\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+
+    in = fmemopen(argv[1], strlen(argv[1]), "r");
+    if (in == NULL)
+        handle_error("fmemopen");
+
+
+    out = open_memstream(&ptr, &size);
+    if (out == NULL)
+        handle_error("open_memstream");
+
+
+    for (;;) {
+        s = fscanf(in, "%d", &v);
+        if (s <= 0)
+            break;
+
+
+        s = fprintf(out, "%d ", v * v);
+        if (s == -1)
+            handle_error("fprintf");
+    }
+
+
+    fclose(in);
+    fclose(out);
+
+
+    printf("size=%zu; ptr=%s\n", size, ptr);
+
+
+    free(ptr);
+    exit(EXIT_SUCCESS); }  
