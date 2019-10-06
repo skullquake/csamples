76dc3cbcafb311cfc5185a30e634a184dd6216e7
skullquake <ockert8080@gmail.com>
2019-10-06 22:14:47 +0200

    daemon init.d

--- src/daemon/src/a.c
+++ src/daemon/src/a.c
@@ -1,9 +1,10 @@
 #include<syslog.h>
 #include<stdlib.h>
 #include<stdbool.h>
-void process(){
-	syslog(LOG_NOTICE,"Writing to my Syslog");
-}
+int main(int,char**);
+void handler(int);
+void process();
+int exitNow=0;
 int main(int argc,char** argv){
 	setlogmask(LOG_UPTO(LOG_NOTICE));
 	openlog ("testd",LOG_CONS|LOG_PID|LOG_NDELAY,LOG_LOCAL1);
@@ -18,10 +19,22 @@ int main(int argc,char** argv){
 	close(STDIN_FILENO);
 	close(STDOUT_FILENO);
 	close(STDERR_FILENO);
-	while(true){
+	signal(SIGTERM,handler);
+	signal(SIGHUP,SIG_IGN);
+	while(1){
 		process();
+		if(exitNow!=0)break;
 		sleep(1);
 	}
+	syslog(LOG_INFO,"Ending Daemon");
 	closelog();
 	return(EXIT_SUCCESS);
 }
+void handler(int s){
+	syslog(LOG_NOTICE,"Exit signal received!");
+	exitNow=1;
+}
+void process(){
+	syslog(LOG_NOTICE,"Writing to my Syslog");
+}
+
