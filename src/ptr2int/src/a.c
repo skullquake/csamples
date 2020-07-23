6dd2c7469c6e794f279c699bb8d6d2d62e2f5c4c
skullquake <ockert8080@gmail.com>
2020-07-23 06:49:01 -0700

    .

--- src/ptr2int/src/a.c
+++ src/ptr2int/src/a.c
@@ -4,17 +4,18 @@
 #include<assert.h>
 int main(void){
 	unsigned long a=42;
-	printf(" &a: %p\n", &a);
+	printf("                  &a: %p\n", &a);
 	unsigned long*ap=(unsigned long*)&a;
-	printf("&ap: %p\n",&ap);
-	printf(" ap: %p\n", ap);
-	printf("*ap: %zu\n",*ap);
+	printf("                 &ap: %p\n",&ap);
+	printf("                  ap: %p\n", ap);
+	printf("                 *ap: %zu\n",*ap);
 	unsigned long b=(unsigned long)ap;
-	printf(" &b: %p\n", &b);
-	printf("  b: 0x%lx\n", b);
+	printf("                  &b: %p\n", &b);
+	printf("                   b: 0x%lx\n", b);
+	printf("*((unsigned long*)b): %zu\n",*((unsigned long*)b));
 	ap=(unsigned long*)b;
-	printf("&ap: %p\n",&ap);
-	printf(" ap: %p\n", ap);
-	printf("*ap: %zu\n",*ap);
+	printf("                 &ap: %p\n",&ap);
+	printf("                  ap: %p\n", ap);
+	printf("                 *ap: %zu\n",*ap);
 	return EXIT_SUCCESS;
 }
